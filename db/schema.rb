16a62c2a5839657f9a6ad8cc0b9d0b13de296cd3
clement bonnemaire <clement.bonnemaire@gmail.com>
2018-07-24 22:26:01 +0800

    created associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180724133811) do
+ActiveRecord::Schema.define(version: 20180724141851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "baskets", force: :cascade do |t|
+    t.float "total_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "orders", force: :cascade do |t|
     t.datetime "date"
@@ -21,6 +27,9 @@ ActiveRecord::Schema.define(version: 20180724133811) do
     t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "basket_id"
+    t.integer "product_id"
+    t.integer "user_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -39,6 +48,7 @@ ActiveRecord::Schema.define(version: 20180724133811) do
     t.float "rate"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "order_id"
   end
 
   create_table "users", force: :cascade do |t|
